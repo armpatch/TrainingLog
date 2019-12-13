8bb973c7b7b78e077403cc4dcaa871d1dbb07fd3
Aaron <ampatch@gmail.com>
2019-12-13 13:02:59 -0500

    Added initial files for main activity

---
+++ app/src/main/java/com/armpatch/android/workouttracker/MainActivity.java
@@ -0,0 +1,32 @@
+package com.armpatch.android.workouttracker;
+
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
